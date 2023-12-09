4569d5ad0ea771da29667430054aebac562ca023
Jayakrishnan Ashok <jayakrishnanashok@gmail.com>
2023-12-10 05:28:38 +0530

    Added multiple logins

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import { useContext, useEffect } from 'react'
 import Dashboard from '@/components/Dashboard';
 import { commentCrawdContext } from '@/context/Firebase/BaseContext';
+import Link from 'next/link'
+import { FaFacebookF, FaLinkedinIn, FaGoogle, FaRegEnvelope } from 'react-icons/fa';
+import { MdLockOutline } from 'react-icons/md';
 
 export default function Home() {
   
@@ -24,7 +27,14 @@ export default function Home() {
       themecolor: `text-[#7335c4]`,
       signIn1: `py-32`,
       signInHeading: `text-3xl font-bold text-[#7335c4] mb-2`,
-      signInHeading2: `border-2 border-[#7335c4] rounded-full px-12 py-2 inline-block font-semibold top-10 hover:cursor-pointer`
+      signInHeading2: `border-2 border-[#7335c4] rounded-full px-12 py-2 inline-block font-semibold top-10 hover:cursor-pointer mt-3`,
+      under: `border-2 w-10 border-[#7335c4] inline-block mb-2`,
+      social: `flex justify-center my-2`,
+      facebook: `text-sm `,
+      linkFacebook: `border-2 border-gray-200 rounded-full p-3 mx-1`,
+      loginSection: `flex flex-col items-center`,
+      loginAdjust: `bg-gray-100 w-64 p-2 flex items-center mb-3`,
+      inputField: `bg-gray-100 outline-none text-sm flex-1`,
     }
   return (
     <div>
@@ -39,18 +49,41 @@ export default function Home() {
             <div className={styles.content}>
               <div className={styles.signInSection}>
                 <div className={styles.companyName}>
-                  <span className={styles.themecolor}>CRAAAAWD</span>Awards
+                  <span className={styles.themecolor}>CRAAAAWARD</span> Awards
                 </div>
                 <div className={styles.signIn1}>
                   <h2 className={styles.signInHeading}>Sign into Twitter</h2>
-                  <a className={styles.signInHeading2} onClick={handleUserAuth} href='#'>Sign In!</a>
+                  <div className={styles.under}></div>
+                  <div className={styles.social}>
+                    <Link href="#" className={styles.linkFacebook}>
+                      <FaFacebookF className={styles.facebook}/>
+                    </Link>
+                    <Link href="#" className={styles.linkFacebook}>
+                      <FaLinkedinIn className={styles.facebook}/>
+                    </Link>
+                    <Link href="#" className={styles.linkFacebook}>
+                      <FaGoogle className={styles.facebook}/>
+                    </Link>
+                  </div>
+                  <p className='text-gray-400 my-3'>or Use your email Account!</p>
+                  <div className={styles.loginSection}>
+                    <div className={styles.loginAdjust}>
+                      <FaRegEnvelope className='text-gray-400 m-2' />
+                      <input type='email' name='email' placeholder='email!' className={styles.inputField} />
+                    </div>
+                    <div className={styles.loginAdjust}>
+                      <MdLockOutline className='text-gray-400 m-2' />
+                      <input type='password' name='email' placeholder='Password' className={styles.inputField} />
+                    </div>
+                  </div>
+                  <a className={styles.signInHeading2} onClick={handleUserAuth} href='#'>Sign In- Demo!</a>
                 </div>
               </div>
               <div className={styles.signInContentSection}>
                 <h2 className={styles.signInWelcome}>Hello Friend!</h2>
                 <div className={styles.signInSubWelcome}></div>
                   <p className={styles.signInSubWelcomeInfo1}>Connect your twitter account and start journey with us.</p>
-                  {/* <a href="#" className={styles.signInSubWelcomeInfo2}>Sign Up!</a> */}
+                  <a href="#" className={styles.signInSubWelcomeInfo2}>Sign Up!</a>
               </div>
             </div>
           </div>
